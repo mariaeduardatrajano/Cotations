0f99fbf437e1a38dbb498e038cb597a96160a278
mariaeduardatrajano <maria.trajano@academico.ifpb.edu.br>
2023-12-18 09:16:10 -0300

    (feat): divindo o arquivo

---
+++ functions.py
@@ -0,0 +1,9 @@
+import requests
+
+def get_cotacao():
+  request = requests.get('https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL').json()
+  cotacao_dolar = request['USDBRL']['bid']
+  cotacao_euro = request['EURBRL']['bid']
+  cotacao_btc =  request['BTCBRL']['bid']
+
+  return {'$' : cotacao_dolar, '€' : cotacao_euro, 'BTC' : cotacao_btc}
